dd99eac9ab8457f10ceff6b02158a2b7d2c6f717
Rafael Cadena <rafaeljcadena@gmail.com>
2019-05-12 21:53:33 -0300

    adjusting gitignore

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const publicPath = path.join(__dirname, '..', 'public')
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath))
 app.get('*', (request, response) => {
   response.sendFile(path.join(publicPath, 'index.html'));
 })
 
-app.listen(3000, () => console.log('Server is up'))
+app.listen(port, () => console.log('Server is up'))
